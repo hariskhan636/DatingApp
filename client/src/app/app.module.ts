4d21107b2407e6aa1b5c2d90db0ca2cb57f5b505
haris.khan636 <harisjriv@gmail.com>
2023-06-08 16:01:36 +0500

    persistent login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [AppComponent, NavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn!: boolean;
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit() {
+    this.getCurrentUser();
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (res) => (console.log(res), (this.loggedIn = true)),
+      error: (err) => console.log(err),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(
+      (user) => {
+        this.loggedIn = !!user;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
